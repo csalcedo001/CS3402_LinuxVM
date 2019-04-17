8981cf52965883367a9fb872186c0aa55e9feddd
Cesar Salcedo <cesar.salcedo@utec.edu.pe>
2019-04-17 04:56:50 +0000

    Finished lab 3.1

--- semana03/measurements/main.c
+++ semana03/measurements/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <time.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
 
 #define TESTS 20
 
@@ -27,6 +30,51 @@ int main (void) {
 		getpid();
 		clock_gettime(CLOCK_MONOTONIC_RAW, &end);
 		totalTime += end.tv_nsec - start.tv_nsec;
+	}
+
+	printf("%.2f ns\n", totalTime / TESTS);
+	
+	totalTime = 0;
+
+	for (int i = 0; i < TESTS; ++i) {
+		char write_msg = '\n';
+		char read_msg;
+		int fd[2];
+		pid_t pid;
+
+		struct timespec start, end;
+	
+		if (pipe(fd) == -1) {
+			fprintf(stderr, "Pipe failed");
+			return 1;
+		}
+	
+		pid = fork();
+	
+		if (pid < 0) {
+			fprintf(stderr, "Fork failed");
+			return 1;
+		}
+		else if (pid > 0) {
+			close(fd[0]);
+	
+			clock_gettime(CLOCK_MONOTONIC_RAW, &start);
+			write(fd[1], &write_msg, 1);
+	
+			close(fd[1]);
+			
+			wait(NULL);
+			clock_gettime(CLOCK_MONOTONIC_RAW, &end);
+
+			totalTime += start.tv_nsec - end.tv_nsec;
+		}
+		else {
+			close(fd[1]);
+	
+			read(fd[0], &read_msg, 1);
+	
+			close(fd[0]);
+		}
 	}
 
 	printf("%.2f ns\n", totalTime / TESTS);
